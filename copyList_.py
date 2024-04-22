079088c9a88272b036c109b1e72913e7a913e641
AliZainoul <ali.zainoul.az@gmail.com>
2024-04-22 20:56:48 +0200

    Adding a file

---
+++ copyList_.py
@@ -0,0 +1,27 @@
+import copy
+
+l1 = [1, 2, 3]
+l2 = l1
+l3 = l1.copy()
+l4 = l1[:]
+l5 = copy.deepcopy(l1)
+# Before modification of l1
+print("# Before modification of l1")
+for el in [l1, l2, l3, l4, l5]:
+    print(el)
+
+l1[0] = 10
+# After modification of l1
+print("# After modification of l1")
+for el in [l1, l2, l3, l4, l5]:
+    print(el)
+
+
+'''
+# Displaying all lists
+print("Original list:", l1)  # Output: Original list: [10, 2, 3]
+print("Shallow copy (l2):", l2)  # Output: Shallow copy (l2): [10, 2, 3]
+print("Shallow copy (l3):", l3)  # Output: Shallow copy (l3): [1, 2, 3]
+print("Shallow copy (l4):", l4)  # Output: Shallow copy (l4): [1, 2, 3]
+print("Deep copy (l5):", l5)  # Output: Deep copy (l5): [1, 2, 3]
+'''
