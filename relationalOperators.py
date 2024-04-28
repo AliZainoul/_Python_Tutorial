4ca1e2ba3ce6cf2e1c039fcc4ca420358190bd53
AliZainoul <ali.zainoul.az@gmail.com>
2024-04-28 13:42:22 +0200

    Adding relationalOp file

---
+++ relationalOperators.py
@@ -0,0 +1,41 @@
+# Example of relational operators in Python
+
+# Definition of two variables
+a = 7
+b = 3
+
+# Equal to operator
+if a == b:
+    print(f"{a} is equal to {b}")
+else:
+    print(f"{a} is not equal to {b}")
+
+# Not equal to operator
+if a != b:
+    print(f"{a} is not equal to {b}")
+else:
+    print(f"{a} is equal to {b}")
+
+# Greater than operator
+if a > b:
+    print(f"{a} is greater than {b}")
+else:
+    print(f"{a} is not greater than {b}")
+
+# Less than operator
+if a < b:
+    print(f"{a} is less than {b}")
+else:
+    print(f"{a} is not less than {b}")
+
+# Greater than or equal to operator
+if a >= b:
+    print(f"{a} is greater than or equal to {b}")
+else:
+    print(f"{a} is not greater than or equal to {b}")
+
+# Less than or equal to operator
+if a <= b:
+    print(f"{a} is less than or equal to {b}")
+else:
+    print(f"{a} is not less than or equal to {b}")
