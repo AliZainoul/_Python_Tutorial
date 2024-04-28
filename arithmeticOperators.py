c5b18fe910128f70831a3018c34f6a6328178d36
AliZainoul <ali.zainoul.az@gmail.com>
2024-04-28 13:33:15 +0200

    Adding arithmeticOp

---
+++ arithmeticOperators.py
@@ -0,0 +1,38 @@
+# Example of basic arithmetic operations in Python
+
+# Definition of two variables x and y
+x = 7
+y = 3
+result_addition = x + y
+print(f"Addition: {x} + {y} = {result_addition}")  
+# Displays: Addition: 7 + 3 = 10
+
+# Subtraction
+result_subtraction = x - y
+print(f"Subtraction: {x} - {y} = {result_subtraction}")  
+# Displays: Subtraction: 7 - 3 = 4
+
+# Multiplication
+result_multiplication = x * y
+print(f"Multiplication: {x} * {y} = {result_multiplication}")  
+# Displays: Multiplication: 7 * 3 = 21
+
+# Division
+result_division = x / y
+print(f"Division: {x} / {y} = {result_division}")  
+# Displays: Division: 7 / 3 = 2.3333333333333335
+
+# Entire Division (Euclidian Division)
+result_entire_division2 = x // 2
+print(f"Entire Division: {x} // {2} = {result_entire_division2}") 
+# Displays: Entire Division: 7 // 2 = 3
+
+# Entire Division (Euclidian Division)
+result_entire_division = x // y
+print(f"Entire Division: {x} // {y} = {result_entire_division}")  
+# Displays: Entire Division: 7 // 3 = 2
+
+# Modulo
+result_modulo = x % y
+print(f"Modulo: {x} % {y} = {result_modulo}")  
+# Displays: Modulo: 7 % 3 = 1
