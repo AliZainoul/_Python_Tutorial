133702127fa9f3c22b541acb636bea9823376936
AliZainoul <ali.zainoul.az@gmail.com>
2024-07-15 16:07:02 +0200

    addding add

---
+++ examples/add.py
@@ -0,0 +1,9 @@
+def add(x: int, y: int) -> int: 
+    """
+    Add two numbers .
+    Parameters :
+    x (int): The first number. y ( int ) : The second number.
+    Returns :
+    int: The sum of the two numbers. 
+    """
+    return x+y
