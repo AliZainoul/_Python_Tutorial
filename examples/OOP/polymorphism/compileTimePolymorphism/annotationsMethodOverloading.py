3fe685cf67f51be7b13f32859dca6712dba90688
AliZainoul <ali.zainoul.az@gmail.com>
2024-08-06 11:24:54 +0200

    adding exs and modif annot

--- examples/OOP/polymorphism/compileTimePolymorphism/annotationsMethodOverloading.py
+++ examples/OOP/polymorphism/compileTimePolymorphism/annotationsMethodOverloading.py
@@ -1,3 +1,5 @@
+# METHOD OVERLOADING --> COMPILE TIME POLYMORPHISM
+
 class MathOperations:
     @staticmethod
     def add(a: int, b: int) -> int:
@@ -7,5 +9,15 @@ class MathOperations:
     def add(a: float, b: float) -> float:
         return a + b
 
+    @staticmethod
+    def add(a: str, b: str) -> str:
+        return a + b
+    
+    @staticmethod
+    def add(a: list, b: list) -> list:
+        return a + b
 print(MathOperations.add(2, 3))  # Output: 5
 print(MathOperations.add(2.2, 3.3))  # Output: 5.5
+print(MathOperations.add( "Hello ", str(input("Enter your name: "))))  # Output: Hello <Name>
+print(MathOperations.add([1,2,3] , [4,5,6]))  # Output: Hello <Name>
+
