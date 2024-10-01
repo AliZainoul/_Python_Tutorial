45618b8c638773137139f444d76ba605e77b2884
AliZainoul <ali.zainoul.az@gmail.com>
2024-10-01 10:33:51 +0200

    update

--- examples/_module_Import/module.py
+++ examples/_module_Import/module.py
@@ -1,11 +1,11 @@
 # module .py
-def function() -> None:
-    print("Function called")
+def function() -> str:
+    return (f"Function called from module")
 
 if __name__ == "__main__":
 # Code to execute only if this script is run as the main
     print("Module is being *RUN DIRECTLY*")
-    function()
+    print(function())
 else:
 # Code to execute if this script is imported as a module
     print("Module is being *IMPORTED*")
