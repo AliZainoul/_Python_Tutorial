44c35ad923011161c969e4ee4b0f5b6448d6af20
AliZainoul <ali.zainoul.az@gmail.com>
2024-10-04 17:21:58 +0200

    modify file

--- exercices/corrections/TP3/ex18.py
+++ exercices/corrections/TP3/ex18.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Generator
 
 def get_inputs() -> List[str]:
     total_number : int = int(input("How many strings do you want to enter ? "))
@@ -13,6 +13,9 @@ def add_strings(*args: List[str]) -> str:
         result += arg + " "
     return result
 
+def gen_strings(*args: List[str]) -> Generator:
+    return ((f"{arg}") for arg in args)
+
 list_of_strs : List[str] = get_inputs()
 
 result_join_strings = join_strings(*list_of_strs)
@@ -21,27 +24,8 @@ print(result_join_strings)
 result_add_strings = add_strings(*list_of_strs)
 print(result_add_strings)
 
-"""
-# Solution for operator + using generators:
-
-from typing import List, Generator
-
-def get_inputs() -> List[str]:
-    total_number : int = int(input("How many strings do you want to enter ? "))
-    return [input(f"Please enter your {index+1} string : \n") for index in range(total_number)]
-
-def join_strings(*args: List[str]) -> str:
-    return str(" ".join(args))
-
-def add_strings(*args: List[str]) -> Generator:
-    return ((f"{arg}") for arg in args)
-
-list_of_strs : List[str] = get_inputs()
-result_join_strings = join_strings(*list_of_strs)
-print(result_join_strings)
-
-result_add_strings = add_strings(*list_of_strs)
+result_gen_strings = gen_strings(*list_of_strs)
 print(*result_add_strings)
 
-# Please note the usage of the starred operator in the last line !
-"""
+# Please note that the join method is the most efficient one: 
+# https://waymoot.org/home/python_string/
