c3c77dc1dd9fc2356f42611f2b505774a438adb3
AliZainoul <ali.zainoul.az@gmail.com>
2024-10-08 15:03:25 +0200

    adding all and modif some files

--- examples/library.py
+++ examples/library.py
@@ -1,3 +1,25 @@
+"""
+CRUD : Create Read Update Delete
+Projet Final : Créez un programme de gestion de bibliothèque qui inclut les éléments suivants :
+
+• Partie 1 : Définition des fonctions de base
+– Créez une fonction add_book(library, book_name) qui ajoute 
+un livre à la bibliothèque (représentée par un dictionnaire). (CREATE)
+– Créez une fonction remove_book(library, book_name) qui supprime un livre de la bibliothèque. (DELETE)
+– Créez une fonction find_book(library, book_name) qui vérifie si un livre est présent dans la bibliothèque. (READ)
+
+• Partie 2 : Générateurs et itérateurs
+– Créez un générateur available_books(library) qui génère la liste des livres disponibles dans la bibliothèque. (READ)
+
+• Partie 3 : Gestion avancée des livres
+– Utilisez des fonctions anonymes lambda avec map() pour créer une liste des noms des livres en majuscules.
+– Utilisez filter() pour créer une liste des livres dont le nom commence par une lettre spécifique.
+
+• Partie 4 : Exécution dynamique
+– Utilisez eval() pour permettre à l’utilisateur de saisir une expression conditionnelle pour rechercher des livres (par ex- emple, tous les livres contenant "Python").
+– Utilisez exec() pour permettre à l’utilisateur d’ajouter dy- namiquement une nouvelle fonctionnalité au programme (par exemple, ajouter une nouvelle méthode de tri pour les livres).
+"""
+
 def print_line(s: str) -> None:
     """
     Prints a separation line with the given text.
@@ -37,31 +59,36 @@ books = {
     }
 }
 
-def print_books(books: dict) -> None:
+def print_books(library: dict) -> None:
     """
     Prints all the books in the library.
     
     Args:
-    books (dict): A dictionary of books with their details.
+    library (dict): A dictionary of books with their details.
     
     Returns:
     None
     """
-    for book in books:
-        print(f"{book}, {books[f'{book}']}")
+    for book_name in library:
+        print(f"{book_name}, {library[f'{book_name}']}")
 
-def available_books(books: dict):
+def available_books(library: dict):
     """
     Yields the available books and their details in the library.
     
     Args:
-    books (dict): A dictionary of books with their details.
+    library (dict): A dictionary of books with their details.
     
     Yields:
     tuple: A tuple containing the book name and its details.
     """
-    for book_name, book_name_specs in books.items():
+    for book_name, book_name_specs in library.items():
         yield book_name, book_name_specs
+
+def print_all(library: dict) -> None:
+    for book in available_books(library):
+        print(book)
+
 
 def add_book(library: dict, book_name: str) -> None:
     """
@@ -96,6 +123,32 @@ def find_book(library: dict, book_name: str):
     """
     return library.get(book_name, "Not found")
 
+def _find_book(library: dict, prompt: str):
+    """
+    Finds a book in the library by its name.
+    
+    Args:
+    library (dict): The library dictionary.
+    book_name (str): The name of the book to find.
+    
+    Returns:
+    dict or str: The book details if found, otherwise "Not found".
+    """
+
+    # l = []
+    # for book_name, book_specs in library.items():
+    #     if prompt in book_name:
+    #         l.append((book_name, book_specs))
+        
+    #     for author, year in book_specs.items():
+    #         if prompt in author or prompt in book_name:
+    #             l.append((book_name, book_specs))
+
+    # return l
+
+    return [(book_name, book_specs) for (book_name, book_specs) in library.items() if prompt in book_name]
+# TODO nested comprehension lists
+
 def is_present(library: dict, book_name: str) -> bool:
     """
     Checks if a book is present in the library.
@@ -122,33 +175,39 @@ def remove_book(library: dict, book_name: str):
     """
     return library.pop(book_name, "Book not found.")
 
+
+
 # BEGIN MAIN
 if __name__ == "__main__":
     print_line("Printing Books initially: ")
-    for book in available_books(books):
-        print(book)
+    print_all(books)
 
     a_book = "Harry Potter"
     print_line(f"Adding book: {a_book}")
     add_book(books, a_book)
 
     print_line(f"Printing Books after adding book: {a_book}")
-    for book in available_books(books):
-        print(book)
+    print_all(books)
+
 
     target_book = "Les Misérables"
     print_line(f"Searching book: {target_book}")
     print(find_book(books, target_book))
 
-    target_book = "Les"
+    target_book = "É"
     print_line(f"Searching book: {target_book}")
+    print_line(f"with function FIND_BOOK uses get method of DICT")
     print(find_book(books, target_book))
+
+    target_book = "É"
+    print_line(f"Searching book: {target_book}")
+    print_line(f"with function _FIND_BOOK uses IN operator with prompt")
+    print(_find_book(books, target_book))
 
     book_to_delete = "Harry Potter"
     print_line(f"Deleting book: {book_to_delete}")
-    print(remove_book(books, book_to_delete))
-    for book in available_books(books):
-        print(book)
+    print(f"{remove_book(books, book_to_delete)} \n")
+    print_all(books)
 
     book_to_delete = "x"
     print_line(f"Tempting to delete book: {book_to_delete}")
